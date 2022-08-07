2b27fc9204e9b74e1e675a642727db9ab3614cf7
alex-skakun <alex.skakun@me.com>
2022-08-07 21:13:12 +0300

    added type wrapper for useFunction result

--- src/useFunction.ts
+++ src/useFunction.ts
@@ -2,21 +2,25 @@ import { useRef } from 'react';
 import { useOnce } from './useOnce';
 
 
+interface MemoizedFunction<T extends (...args: any[]) => any> extends CallableFunction {
+  (...args: Parameters<T>): ReturnType<T>;
+}
+
 /**
  * This hook provides stable function that invokes passed callback.
  * It doesn't require dependencies, because callback is always up-to-date.
  * Use it as alternative for `useCallback()`.
  */
-export function useFunction<T extends (...args: any[]) => any>(fn: T): T {
+export function useFunction<T extends (...args: any[]) => any>(fn: T): MemoizedFunction<T> {
   const fnRef = useRef<T>(fn);
 
   fnRef.current = fn;
 
-  return useOnce<T>(() => {
+  return useOnce<MemoizedFunction<T>>(() => {
     return ((...args: Parameters<T>): ReturnType<T> => {
       const { current: originalFunction } = fnRef;
 
       return originalFunction(...args);
-    }) as T;
+    });
   });
 }
